13b214e0036c6293bd4c6b42655c8a2354bf4c72
kim <395362774qq.com>
2017-05-17 19:51:20 +0000

    fix

--- base/BackendController.php
+++ base/BackendController.php
@@ -11,11 +11,11 @@ class BackendController extends Controller {
 
     protected function beforeAction(){
         parent::beforeAction();
-//        if(!isset($_SESSION['user_id'])){
-//            $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-//            setcookie('refer',$url);
-//            header("location:/index.php?r=auth/getCode");
-//            exit;
-//        }
+        if(!isset($_SESSION['user_id'])){
+            $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+            setcookie('refer',$url);
+            header("location:/index.php?r=auth/getCode");
+            exit;
+        }
     }
 }
